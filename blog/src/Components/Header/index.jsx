e0d019dc30fe45af1a3501a3c53502d50a0ae143
Oumar <oumarmauret@gmail.com>
2023-03-05 13:16:11 +0000

    finish responve design

--- blog/src/Components/Header/index.jsx
+++ blog/src/Components/Header/index.jsx
@@ -3,7 +3,11 @@ import { Link } from "react-router-dom";
 import Logo from "../../styles/assets/icons/logo_alter_ego.svg";
 
 const Header = () => {
-   const [open, setOpen] = useState(false);
+   const [showLink, setShowLink] = useState(false);
+
+   const handleShowLink = () => {
+      setShowLink(!showLink);
+   };
 
    return (
       <header class="header">
@@ -16,11 +20,20 @@ const Header = () => {
                />
             </a>
          </div>
-         <nav class="header__nav">
-            <Link to="/">Accueil</Link>
-            <Link to="/about">A propos de nous</Link>
-            <Link to="/blog">Blog</Link>
-            <Link to="/contact">Contactez-nous</Link>
+         <nav
+            className={`header__nav ${showLink ? "show__nav" : "hide__nav"} `}
+         >
+            <ul>
+               <Link to="/">Accueil</Link>
+               <Link to="/about">A propos de nous</Link>
+               <Link to="/blog">Blog</Link>
+               <Link to="/contact">Contactez-nous</Link>
+            </ul>
+
+            <span
+               onClick={handleShowLink}
+               className={`header__burger__bar ${showLink ? "open" : "close"}`}
+            ></span>
          </nav>
       </header>
    );
