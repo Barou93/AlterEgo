cc2d4f468ff0b8105c846d455c16f2ce599c69f5
Oumar <oumarmauret@gmail.com>
2023-04-13 17:26:09 +0000

    update site web

--- server.js
+++ server.js
@@ -43,6 +43,13 @@ const corsOptions = {
 };
 */
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
 app.use(cors());
 const {requireAuth} = require('./middleware/auth.middleware');
 app.get('/jwtid', requireAuth, (req, res) => {
