d6c3f1742ff3f77ffab2691a20a6cae426eff4ad
Oumar <oumarmauret@gmail.com>
2023-04-14 10:27:33 +0000

    Update serve

--- server.js
+++ server.js
@@ -39,12 +39,12 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-// app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-//   next();
-// });
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
 
 
 const {requireAuth} = require('./middleware/auth.middleware');
