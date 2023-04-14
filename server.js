ce22f401659530f6ddf6aba4c96f34d507a762e7
Oumar <oumarmauret@gmail.com>
2023-04-14 09:46:21 +0000

    update server.js

--- server.js
+++ server.js
@@ -12,9 +12,11 @@ const compression = require('compression');
 const adminRoutes = require('./routes/admin.routes');
 const articleRoutes = require('./routes/article.routes');
 const infosRoutes = require('./routes/information.routes');
+
 const app = express();
 
-require('dotenv').config();
+require('dotenv').config('./.env');
+
 app.use(compression());
 
 
@@ -29,22 +31,22 @@ app.use(cookieParser());
 app.use(helmet());
 
 
-// const corsOptions = {
-//   origin: process.env.FRONT_URL,
-//   credentials: true,
-//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-//   // preflightContinue: false,
-// };
+const corsOptions = {
+  origin: process.env.FRONT_URL,
+  credentials: true,
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  preflightContinue: false,
+};
+app.use(cors(corsOptions));
+
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+//   next();
+// });
 
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
-});
-
-app.use(cors());
 const {requireAuth} = require('./middleware/auth.middleware');
 app.get('/jwtid', requireAuth, (req, res) => {
 
