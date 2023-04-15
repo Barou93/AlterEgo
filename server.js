e7bee7ca5b2f68a5768d2370c9f8e70794efe045
Oumar <oumarmauret@gmail.com>
2023-04-15 03:28:13 +0000

    update server file

--- server.js
+++ server.js
@@ -20,10 +20,10 @@ require('dotenv').config('./.env');
 app.use(compression());
 
 
-app.use(express.static(path.resolve(__dirname, '/public')));
-// app.get('/*', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, '/public', 'index.html'));
-// });
+app.use(express.static(path.resolve(__dirname, './public')));
+app.get('/*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './public', 'index.html'));
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
