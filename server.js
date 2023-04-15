71ea9283902534f41176799beb5d73b82ce8e344
Oumar <oumarmauret@gmail.com>
2023-04-15 03:16:27 +0000

    update server.js

--- server.js
+++ server.js
@@ -21,9 +21,9 @@ app.use(compression());
 
 
 app.use(express.static(path.resolve(__dirname, '/public')));
-app.get('/*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '/public', 'index.html'));
-});
+// app.get('/*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, '/public', 'index.html'));
+// });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
